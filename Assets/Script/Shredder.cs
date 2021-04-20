d25348427f1f666cc409f1693e8ef19310cf2ed4
rrbyn <robin.kukk23@gmail.com>
2021-04-20 22:21:35 +0300

    first commit

---
+++ Assets/Script/Shredder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collisionTrigger)
+    {
+        Destroy(collisionTrigger.gameObject);
+    }
+}
